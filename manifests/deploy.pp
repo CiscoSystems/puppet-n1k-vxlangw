bc4b9c6816c3bac117674ee337150d1b9a9f92a2
kganesa2 <kganesa2@cisco.com>
2013-09-12 21:55:46 -0700

    updated the name of Gw image

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -16,10 +16,11 @@ class n1k-vxlangw::deploy {
   }
 
   $qcowpath = "/etc/vxlangw/$b"
+  $image_name = regsubst($b,'.disk1','')
   exec {"add_glance":
-        command => "/usr/bin/glance image-create --name=$b --is-public=true --property hw_vif_model=e1000 --property hw_disk_bus=ide --property hw_cdrom_bus=ide --container-format=ovf --disk-format=qcow2 < $qcowpath",
+        command => "/usr/bin/glance image-create --name=$image_name --is-public=true --property hw_vif_model=e1000 --property hw_disk_bus=ide --property hw_cdrom_bus=ide --container-format=ovf --disk-format=qcow2 < $qcowpath",
         environment => ["OS_USERNAME=admin","OS_TENANT_NAME=admin","OS_PASSWORD=$os_password","OS_AUTH_URL=http://$controller_ip:5000/v2.0/"],
-        unless => "/usr/bin/glance index | grep -c $b",
+        unless => "/usr/bin/glance index | grep -c $image_name",
      }
 
 File["/etc/vxlangw"] ->  File["$isopath"] -> Exec["add_glance"]
