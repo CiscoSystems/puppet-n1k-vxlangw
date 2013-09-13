f5f5f3a9b7c3cac11c7104bea2cea113cf8ed530
kganesa2 <kganesa2@cisco.com>
2013-09-13 08:11:40 -0700

    changed the class to define type to execute after the glance services are installed

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class n1k-vxlangw (
+define n1k-vxlangw (
     $gw_isoimage,
     $os_password,
     $controller_ip)
@@ -12,6 +12,31 @@ class n1k-vxlangw (
   $dirpath = "/var/spool/vxlangw/"
   $isopath = "/etc/vxlangw/$b"
 
-  include n1k-vxlangw::deploy
+  #include n1k-vxlangw::deploy
+
+  file { '/etc/vxlangw':
+         owner => 'root',
+         group => 'root',
+         mode  => '664',
+         ensure => directory,
+  }
+
+  file { $isopath:
+         owner => 'root',
+         group => 'root',
+         mode => '666',
+         source => "puppet:///files/$b",
+         require => File['/etc/vxlangw']
+  }
+
+  $qcowpath = "/etc/vxlangw/$b"
+  $image_name = regsubst($b,'.disk1','')
+  exec {"add_glance":
+        command => "/usr/bin/glance image-create --name=$image_name --is-public=true --property hw_vif_model=e1000 --property hw_disk_bus=ide --property hw_cdrom_bus=ide --container-format=ovf --disk-format=qcow2 < $qcowpath",
+        environment => ["OS_USERNAME=admin","OS_TENANT_NAME=admin","OS_PASSWORD=$os_password","OS_AUTH_URL=http://$controller_ip:5000/v2.0/"],
+        unless => "/usr/bin/glance index | grep -c $image_name",
+  }
+
+  File["/etc/vxlangw"] ->  File["$isopath"] -> Exec["add_glance"]
 
 }
